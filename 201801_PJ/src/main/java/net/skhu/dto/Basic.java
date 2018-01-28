65bb92a839e87fe905eb60670928ecc2a5729d79
dayoni <callmey@nate.com>
2018-01-29 00:11:42 +0900

    comment, article dto 추가

--- 201801_PJ/src/main/java/net/skhu/dto/Basic.java
+++ 201801_PJ/src/main/java/net/skhu/dto/Basic.java
@@ -1,8 +1,10 @@
 package net.skhu.dto;
 
+import lombok.Data;
+
+@Data
 public class Basic {
 	String recipe_id;
-
 	String recipe_nm_ko;
 	String sumry;
 	String nation_nm;
@@ -10,52 +12,4 @@ public class Basic {
 	String calorie;
 	String img_url;
 	String det_url;
-	public String getRecipe_id() {
-		return recipe_id;
-	}
-	public void setRecipe_id(String recipe_id) {
-		this.recipe_id = recipe_id;
-	}
-	public String getRecipe_nm_ko() {
-		return recipe_nm_ko;
-	}
-	public void setRecipe_nm_ko(String recipe_nm_ko) {
-		this.recipe_nm_ko = recipe_nm_ko;
-	}
-	public String getSumry() {
-		return sumry;
-	}
-	public void setSumry(String sumry) {
-		this.sumry = sumry;
-	}
-	public String getNation_nm() {
-		return nation_nm;
-	}
-	public void setNation_nm(String nation_nm) {
-		this.nation_nm = nation_nm;
-	}
-	public String getTy_nm() {
-		return ty_nm;
-	}
-	public void setTy_nm(String ty_nm) {
-		this.ty_nm = ty_nm;
-	}
-	public String getCalorie() {
-		return calorie;
-	}
-	public void setCalorie(String calorie) {
-		this.calorie = calorie;
-	}
-	public String getImg_url() {
-		return img_url;
-	}
-	public void setImg_url(String img_url) {
-		this.img_url = img_url;
-	}
-	public String getDet_url() {
-		return det_url;
-	}
-	public void setDet_url(String det_url) {
-		this.det_url = det_url;
-	}
 }
